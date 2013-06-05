57e0027a5339ec4ac902eccf4db363ae1ca73b48
Nils Edison <ramon.zolliker@gmail.com>
2013-06-05 10:49:44 +0200

    js

--- javascripts/vendor/setup.js
+++ javascripts/vendor/setup.js
@@ -4,10 +4,68 @@ var query = URI.parseQuery(window.location.search);
 var model = new SimpleModel(query);
 // Store the model's values in the URL when the user navigates to a new page
 $('[data-stateful-link]').on('click', function(evt) {
+
+	evt.preventDefault();
+
   var uri = new URI(evt.currentTarget.href);
   uri.search(URI.buildQuery(model._fields));
-  evt.currentTarget.href = uri.href();
+
+
+  // evt.currentTarget.href = uri.href();
+
+  var appContainer = $('#app-container')
+
+
+  // appContainer.css({position: 'absolute'})
+
+  //  appContainer.animate({
+  //   left: -appContainer.width()
+  // }, {
+  //   duration: 2000,
+  //   complete: function() {
+      
+
+  //     window.location = uri.href();
+  //   }
+  // });
+	console.log("kjhdshf")
+	appContainer.get(0).addEventListener("transitionend", function() {
+		console.log('fini....')
+      window.location = uri.href();
+	}, true);
+
+	appContainer.addClass('ramonsFancyFadeOut');
+
+
 });
 // SETUP END /////////////////////////////////////////////////////////////
 
 
+
+// $('[data-link-left]').on('click', function(evt) {
+
+// 	evt.preventDefault();
+
+//   var uri = new URI(evt.currentTarget.href);
+//   uri.search(URI.buildQuery(model._fields));
+
+
+//   // evt.currentTarget.href = uri.href();
+
+//   var appContainer = $('#app-container')
+//   appContainer.css({position: 'absolute'})
+
+//    appContainer.animate({
+//     left: -appContainer.width()
+//   }, {
+//     duration: 2000,
+//     complete: function() {
+      
+
+//       window.location = uri.href();
+//     }
+//   });
+
+
+
+// });
